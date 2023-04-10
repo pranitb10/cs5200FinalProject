7c7091e0a494458b0e9b2a3a884765f754749675
Vera-wz <83155578+Vera-wz@users.noreply.github.com>
2023-04-09 20:25:41 -0400

    update

--- Schema.sql
+++ Schema.sql
@@ -4,85 +4,91 @@ use Airbnb;
 
 -- the values in table languages and gender_pronouse are not changable
 
-CREATE TABLE IF NOT EXISTS gender_pronous(
-  id INT NOT NULL,
-  pronous VARCHAR(50) NOT NULL,
-  PRIMARY KEY (id)
-);
-
-
 CREATE TABLE IF NOT EXISTS languages (
-  id INT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(50) NOT NULL,
-  PRIMARY KEY (id)
+  `name` VARCHAR(50) NOT NULL,
+  `code` CHAR(2),
+  PRIMARY KEY (`code`)
 );
 
 
-DROP TABLE IF EXISTS  tenantes;
-CREATE TABLE tenantes (
+DROP TABLE IF EXISTS  tenants;
+CREATE TABLE tenants (
 	id INT PRIMARY KEY,
-	name  VARCHAR(20) NOT NULL,
-	email VARCHAR(50) UNIQUE,
-	phone INT,
-    gender INT,
-    language_id INT(4),
-    FOREIGN KEY (language_id) REFERENCES languages(id),
-    FOREIGN KEY (gender) REFERENCES gender_pronous(id)
+	`name`  VARCHAR(20) NOT NULL,
+	email VARCHAR(50) NOT NULL UNIQUE,
+	phone VARCHAR(20) UNIQUE,
+    gender ENUM("Male", "Female", "Others", "Not to tell") DEFAULT "Not to tell",
+    language_code CHAR(2),
+
+	FOREIGN KEY (language_code) REFERENCES languages(`code`) 
+    -- FOREIGN KEY (gender) REFERENCES gender_pronous(id)
 );
 
+-- INSERT tenants(id, `name`, email, language_code)
+-- Value(10, "Ellen", "eell_ww@124.com", "EN");
 
 DROP TABLE IF EXISTS hosts;
 CREATE TABLE `hosts`(
     id INT,
-    host_name VARCHAR(150),
-    email VARCHAR(50) UNIQUE,
-	language_id INT(4),
+    host_name VARCHAR(150) NOT NULL,
+    email VARCHAR(50) NOT NULL UNIQUE,
+    phone VARCHAR(20) UNIQUE,
+    gender ENUM("Male", "Female", "Others", "Not to tell") DEFAULT "Not to tell",
+	language_code CHAR(2),
     PRIMARY KEY(id),
-    FOREIGN KEY (language_id) REFERENCES languages(id)
+    FOREIGN KEY (language_code) REFERENCES languages(`code`)
 );
+
+
+DROP TABLE IF EXISTS world_cities;
+CREATE TABLE IF NOT EXISTS world_cities(
+    city_id INT PRIMARY KEY,
+    city VARCHAR(225) NOT NULL,
+    country VARCHAR(225) NOT NULL
+);
+
 
 DROP TABLE IF EXISTS airbnbs;
 CREATE TABLE airbnbs(
     house_id INT,
 	host_id INT,
     title VARCHAR(100) NOT NULL,
-    country CHAR(60),
-    city CHAR(60),
-    neighbor CHAR(60),
-    street CHAR(60),
-    num_of_rooms INT,
-    num_of_beds INT,
+    city_id INT,
+    address VARCHAR(100),  -- Specific address
+    num_of_rooms INT(3),
+    num_of_beds INT(3),
+    num_parking INT(2),
+    average_rating DECIMAL(3,2) DEFAULT NULL,
+    rating_times INT DEFAULT 0,
+    `description` varchar(10000) DEFAULT NULL,
+    current_price DECIMAL(10,2),
+    current_cleaning_fee DECIMAL(10,2),
     PRIMARY KEY (house_id),
-    FOREIGN KEY (host_id) REFERENCES hosts(id)
+    FOREIGN KEY (host_id) REFERENCES hosts(id),
+    FOREIGN KEY (city_id) REFERENCES world_cities(city_id)
 );
+
 
 DROP TABLE IF EXISTS airbnb_unavailable;
 CREATE TABLE airbnb_unavailable(
-	house_id INT,
+	house_id int,
 	start_date DATE,
     end_date DATE,
-  FOREIGN KEY (house_id) REFERENCES airbnbs(house_id)
+    PRIMARY KEY (house_id, start_date, end_date),
+	FOREIGN KEY (house_id) REFERENCES airbnbs(house_id)
 );
-
+    
 DROP TABLE IF EXISTS orders;
 CREATE TABLE orders(
-	order_num INT,
-    check_in_date date,
-    check_out_date date,
-    price_per_day float,
-    cleaning_fee  float,
+	order_num VARCHAR(10),
+    tenant INT NOT NULL,
+    house_id INT NOT NULL,
+    check_in_date date NOT NULL,
+    check_out_date date NOT NULL,
+    price_per_day DECIMAL(10,2),
+    cleaning_fee  DECIMAL(10,2),
     states ENUM("wait to comfired", "cencaled", "processing", "completed"),
-    primary key (order_num)
+    primary key (order_num),
+    FOREIGN KEY (tenant) REFERENCES tenants(id),
+    FOREIGN KEY (house_id) REFERENCES airbnbs(house_id) 
 );
-
-
--- complex relationship table book
-DROP TABLE IF EXISTS book;
-CREATE TABLE book(
-	tenante INT,
-    airbnb INT,
-    order_num INT,
-    FOREIGN KEY (tenante) REFERENCES tenantes(id) ON DELETE RESTRICT ON UPDATE RESTRICT,
-    FOREIGN KEY (airbnb) REFERENCES airbnbs(house_id),
-    FOREIGN KEY (order_num) REFERENCES orders(order_num)
-)

e70249a19173f90ede6a9e7ea42ed491cb9f9932
Vera-wz <vera.zwy@outlook.com>
2023-04-18 19:42:54 -0400

    Update sample_insert.sql

--- sample_insert.sql
+++ sample_insert.sql
@@ -25,16 +25,16 @@ INSERT INTO hosts (host_name, email, phone, gender, language_code) VALUES
 
 INSERT INTO airbnbs (house_id, `host`, title, city_id, address, num_of_rooms, num_of_beds, num_parking, average_rating, rating_times, description, current_price, current_cleaning_fee)
 VALUES
-(1001, 'john.smith@gmail.com', 'Cozy Apartment in the Heart of the City', 1276974738, '123 Main St.', 2, 1, 1, 2.2, 10, 'This charming apartment is located in the heart of the city and is perfect for a romantic getaway or a small family vacation.', 80.00, 10.00),
-(1002, 'mary.johnson@yahoo.com', 'Spacious Loft with Stunning City Views', 1076094869, '456 Elm St.', 1, 2, 0, 1.6, 5, 'Experience urban living at its finest in this spacious loft with breathtaking city views.', 120.00, 15.00),
-(1003, 'david.lee@hotmail.com', 'Luxury Condo with Pool and Gym Access', 1752708772, '789 Oak St.', 3, 3, 2, 3.0, 15, 'Treat yourself to the ultimate luxury experience in this beautiful condo with access to a pool and gym.', 250.00, 20.00),
-(1004, 'anna.garcia@gmail.com', 'Coastal Cottage with Ocean Views', 1218680506, '321 Beach Rd.', 2, 2, 1, 4.7, 8, 'Escape to the coast and enjoy breathtaking ocean views from this charming cottage.', 150.00, 15.00),
-(1005, 'mohammed.ahmed@yahoo.com', 'Historic Home with Modern Amenities', 1356213628, '987 Main St.', 4, 3, 2, 4.3, 12, 'Experience the best of both worlds in this beautifully restored historic home with all the modern amenities you need for a comfortable stay.', 200.00, 20.00),
-(1006, 'lena.nguyen@gmail.com', 'Charming Bungalow in a Quiet Neighborhood', 1250008059, '567 Maple St.', 2, 1, 1, 5, 6, 'This charming bungalow is the perfect place to relax and unwind in a quiet neighborhood.', 100.00, 10.00),
-(1007, 'lena.nguyen@gmail.com', 'Stylish Studio Apartment in Downtown', 1380805860, '246 Market St.', 1, 1, 0, 3.5, 7, 'Experience city living in style in this modern and chic studio apartment located in the heart of downtown.', 90.00, 10.00),
-(1008, 'sara.kim@hotmail.com', 'Spacious Townhouse with Private Patio', 1840010109, '789 Oak St.', 3, 3, 1, 3.2, 10, 'This spacious townhouse with a private patio is the perfect home away from home for your next family vacation.', 180.00, 20.00),
-(1009, 'ahmed.hassan@gmail.com', 'Modern Apartment with Stunning Views', 1036216956, '357 Hilltop Rd.', 1, 2, 1, 4.7, 4, 'Enjoy breathtaking views from this modern and stylish apartment located on a hilltop.', 120.00, 15.00),
-(1010, 'jasmine.wong@yahoo.com', 'Beautifully Furnished Apartment in the City', 1840019682, '123 Main St.', 2, 2, 1, 3.8, 9, 'This beautifully furnished apartment is located in the heart of the city and is perfect for a comfortable and convenient stay.', 80.00, 15.00);
+(1001, 'john.smith@gmail.com', 'Cozy Apartment in the Heart of the City', 1036074917, '123 Main St.', 2, 1, 1, 2.2, 10, 'This charming apartment is located in the heart of the city and is perfect for a romantic getaway or a small family vacation.', 80.00, 10.00),
+(1002, 'mary.johnson@yahoo.com', 'Spacious Loft with Stunning City Views', 1036074917, '456 Elm St.', 1, 2, 0, 1.6, 5, 'Experience urban living at its finest in this spacious loft with breathtaking city views.', 120.00, 15.00),
+(1003, 'david.lee@hotmail.com', 'Luxury Condo with Pool and Gym Access', 1156228865, '789 Oak St.', 3, 3, 2, 3.0, 15, 'Treat yourself to the ultimate luxury experience in this beautiful condo with access to a pool and gym.', 250.00, 20.00),
+(1004, 'anna.garcia@gmail.com', 'Coastal Cottage with Ocean Views', 1356226629, '321 Beach Rd.', 2, 2, 1, 4.7, 8, 'Escape to the coast and enjoy breathtaking ocean views from this charming cottage.', 150.00, 15.00),
+(1005, 'mohammed.ahmed@yahoo.com', 'Historic Home with Modern Amenities', 1156228865, '987 Main St.', 4, 3, 2, 4.3, 12, 'Experience the best of both worlds in this beautifully restored historic home with all the modern amenities you need for a comfortable stay.', 200.00, 20.00),
+(1006, 'lena.nguyen@gmail.com', 'Charming Bungalow in a Quiet Neighborhood', 1840000455, '567 Maple St.', 2, 1, 1, 5, 6, 'This charming bungalow is the perfect place to relax and unwind in a quiet neighborhood.', 100.00, 10.00),
+(1007, 'lena.nguyen@gmail.com', 'Stylish Studio Apartment in Downtown', 1840000455, '246 Market St.', 1, 1, 0, 3.5, 7, 'Experience city living in style in this modern and chic studio apartment located in the heart of downtown.', 90.00, 10.00),
+(1008, 'sara.kim@hotmail.com', 'Spacious Townhouse with Private Patio', 1840000455, '789 Oak St.', 3, 3, 1, 3.2, 10, 'This spacious townhouse with a private patio is the perfect home away from home for your next family vacation.', 180.00, 20.00),
+(1009, 'ahmed.hassan@gmail.com', 'Modern Apartment with Stunning Views', 1840000455, '357 Hilltop Rd.', 1, 2, 1, 4.7, 4, 'Enjoy breathtaking views from this modern and stylish apartment located on a hilltop.', 120.00, 15.00),
+(1010, 'jasmine.wong@yahoo.com', 'Beautifully Furnished Apartment in the City', 1840034016, '123 Main St.', 2, 2, 1, 3.8, 9, 'This beautifully furnished apartment is located in the heart of the city and is perfect for a comfortable and convenient stay.', 80.00, 15.00);
 
 
 INSERT INTO airbnb_unavailable (house_id, start_date, end_date)
